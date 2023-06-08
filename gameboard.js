71f3217d77763dab7ae8a342c58022ac3df07c7e
Ricky <rzh4321@gmail.com>
2023-06-07 21:49:00 -0400

    - added predictedA and predictedB data members, stores coords that got hit - updated fire() to add to predicted set if hit

--- gameboard.js
+++ gameboard.js
@@ -8,15 +8,22 @@ export default class Gameboard {
     this.shipB = shipB;
     this.turn = shipA;
     this.attackedA = new Set();
+    this.predictedA = new Set();
     this.attackedB = new Set();
-
+    this.predictedB = new Set();
   }
 
   fire(coord) {
-    if ((this.turn === this.shipA && this.attackedA.has(coord)) || (this.turn === this.shipB && this.attackedB.has(coord))) throw new Error("this cell has already been attacked");
+    if (
+      (this.turn === this.shipA && this.attackedA.has(coord)) ||
+      (this.turn === this.shipB && this.attackedB.has(coord))
+    )
+      throw new Error("this cell has already been attacked");
     if (this.turn === this.shipA) this.attackedA.add(coord);
     else this.attackedB.add(coord);
     const hit = this.turn.fire(coord);
+    if (hit && this.turn === this.shipA) this.predictedA.add(coord);
+    else if (hit && this.turn === this.shipB) this.predictedB.add(coord);
     if (this.turn.getSize() === 0) this.displayWinner(this.getTurn());
     this.turn = this.turn === this.shipA ? this.shipB : this.shipA;
     return hit;
